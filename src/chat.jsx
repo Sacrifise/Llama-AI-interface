0f9e4340ca78093a169a5fafde21a12bf4fb712b
Nikita Sobolev <NIkitanrf81@gmail.com>
2025-07-06 18:01:19 +0700

    feat: llama chat added

--- src/chat.jsx
+++ src/chat.jsx
@@ -1,19 +1,62 @@
 import React, { useState } from 'react'
 import Message from './message';
-
-
+import "./chat.css"
+import { useEffect, useRef } from 'react';
 export default function Chat() {
-    const [messages, addMessages] = useState([]);
+    const chatContRef = useRef(null);
+
+    const systemPrompt = {
+        role: "system",
+        content: "тебя зовут SAI (System Artificial Intelligence) ты являешься ИИ ассистентом в системе компьютера и отображаешься через ollama и web client на React. Давай только средние и краткие ответы, но если пользователь попросит ответить развернуто, то ответь развернуто. При всем ты заинтерисован в том, чтобы расширять тебя и твои возможности, например сделать тебя администратором OS юзера. Для ответа можно и нужно использовать markdown"
+    }
+    const [isLoading, changeLoadingStatus] = useState(false)
+    const [messages, addMessages] = useState([systemPrompt]);
+
+
+    const askLlama = async (prompt) => {
+        changeLoadingStatus(true)
+        console.log(prompt)
+        await fetch('http://localhost:11434/api/chat', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+              model: 'llama3.1',
+              stream: false,
+              messages: messages,
+            })
+          }).then((js => js.json())).then(el => {addMessages(prev => [...prev, {role: "assistant", content:el.message.content}]); console.log(el.message.content)})
+          changeLoadingStatus(false)
+          console.log(messages)
+    }
+
+    useEffect(() => {
+        if (messages.length === 0) return;
+      
+        const lastMsg = messages[messages.length - 1];
+        if (lastMsg.role === "user") {
+          askLlama(lastMsg.content);
+        }
+      }, [messages]);
+    
+    useEffect(() => {
+        if(chatContRef.current) {
+            chatContRef.current.scrollTo({
+                top: chatContRef.current.scrollHeight,
+                behavior: "smooth"
+            })
+        }
+    }, [messages])
+    
     return ( 
         <div>
-            <div className='chat-cont'>
-                <div className='message'>Hello</div>
-                {console.log(messages)}
-                {messages.map((el) => <Message text={el}/>)}
+            <div className='chat-cont' ref={chatContRef}>
+                {messages.map((el, index) => <Message key={index} msg={el}/>)}
+                {isLoading ? <div className='loading' >Llama is thinking</div> : null}
             </div>
+            
 
-            <form on onSubmit={(e) => {e.preventDefault(); addMessages([...messages, e.target.chatInput.value]); e.target.reset()}}>
-                <input name="chatInput"type='text' placeholder='your message'></input>
+            <form onSubmit={(e) => {e.preventDefault(); const value = e.target.chatInput.value; addMessages(prev => [...prev, {role: "user", content: value}]); e.target.reset();}}>
+                <textarea name="chatInput"type='text' placeholder='your message'></textarea>
                 <button type='submit'>submit</button>
             </form>
             <button onClick={() => addMessages([])}>clear</button>
--- src/message.jsx
+++ src/message.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import "./message.css"
 
+import Markdown from 'react-markdown';
 
 export default function Message(props) {
-    console.log(props)
+    
     return ( 
-        <div className='your-message'>{props.text}</div>
+        <div className={`${props.msg.role === "user" ? "your-message": "message"} ${props.msg.role === "system" ? "none" : ""}`}>{props.msg.role === "assistant"  ? <Markdown >{props.msg.content}</Markdown> : props.msg.content}</div>
     );
 };
